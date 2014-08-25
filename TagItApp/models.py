16ffd87c0bebfb6aaf04bf8574127012a04e0413
Rachit Gupta <rachitgupta1792@gmail.com>
2014-08-25 22:19:18 +0530

    Added Textual description to models

--- TagItApp/models.py
+++ TagItApp/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 
 class Tag(models.Model):
-	tag_name = models.CharField(max_length=20)
+    tag_name = models.CharField(max_length=20)
+
+    def __unicode__(self):
+        return self.tag_name
 
 class Link(models.Model):
-	link_url = models.URLField()
-	link_tags = models.ManyToManyField(Tag)
+    link_url = models.URLField()
+    link_tags = models.ManyToManyField(Tag)
+
+    def __unicode__(self):
+        return self.link_url
 
 class User(models.Model):
-	user_email = models.EmailField()
-	user_password = models.CharField(max_length=40)		# SHA1 is represented as 40 characters
-	user_links = models.ManyToManyField(Link)
+    user_email = models.EmailField()
+    user_password = models.CharField(max_length=40)     # SHA1 is represented as 40 characters
+    user_links = models.ManyToManyField(Link)
+
+    def __unicode__(self):
+        return self.email
