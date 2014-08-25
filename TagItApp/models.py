2291cc29bebc2e10c62eacd133c80d45b45a6f60
Rachit Gupta <rachitgupta1792@gmail.com>
2014-08-25 22:12:32 +0530

    Added models to the app.

--- TagItApp/models.py
+++ TagItApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+	tag_name = models.CharField(max_length=20)
+
+class Link(models.Model):
+	link_url = models.URLField()
+	link_tags = models.ManyToManyField(Tag)
+
+class User(models.Model):
+	user_email = models.EmailField()
+	user_password = models.CharField(max_length=40)		# SHA1 is represented as 40 characters
+	user_links = models.ManyToManyField(Link)
