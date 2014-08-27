5bfd80136d5ea6e6621122a41a98d6f9d5b2755a
Ankur Gaur <nkrgaur26@gmail.com>
2014-08-27 17:45:09 +0530

    Added basic login and signup views

--- TagItApp/views.py
+++ TagItApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def login(request):
+    return HttpResponse("login")
+
+def signup(request):
+    return HttpResponse("signup")
+
